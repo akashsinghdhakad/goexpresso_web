5edc5bae0d02afd1e1253c2e4b2bd9674b600cf4
Akash dhakad <akash.dhakad@mponline.gov.in>
2025-04-03 00:51:36 +0530

    for demo

--- src/app/pages/traveller/pickup-parcel-page/pickup-parcel-page.component.ts
+++ src/app/pages/traveller/pickup-parcel-page/pickup-parcel-page.component.ts
@@ -7,35 +7,37 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-pickup-parcel-page',
-  imports: [FormsModule,MatSlideToggleModule,MatIconModule,CommonModule],
+  imports: [MatIconModule, CommonModule, FormsModule, MatSlideToggleModule],
   templateUrl: './pickup-parcel-page.component.html',
   styleUrl: './pickup-parcel-page.component.css'
 })
 export class PickupParcelPageComponent {
   isChecked = true;
-   parcelPackageDetailsOptionList: ParcelPackageDetailsOption[] = [
-      {
-        id: 1,
-        title: 'Select Packaging',
-        icon: 'view_in_ar',
-        subtitle: 'Box / Carton'
-      },
-      {
-        id: 2,
-        title: 'Choose Parcel Weight',
-        icon: 'attach_money',
-        subtitle: '0.5 Kg - 2 Kg E',
-      },
-      {
-        id: 3,
-        title: 'Choose Package Content',
-        icon: 'view_in_ar',
-        subtitle: 'Clothes & Personal Items',
-      },
-      {
-        id: 4, 
-        title: 'Delhivery Protect',
-      },
-  
-    ];
+  parcelPackageDetailsOptionList: ParcelPackageDetailsOption[] = [
+    {
+      id: 1,
+      title: 'Select Packaging',
+      icon: 'view_in_ar',
+      subtitle: 'Box / Carton'
+    },
+    {
+      id: 2,
+      title: 'Choose Parcel Weight',
+      icon: 'attach_money',
+      subtitle: '0.5 Kg - 2 Kg E',
+    },
+    {
+      id: 3,
+      title: 'Choose Package Content',
+      icon: 'view_in_ar',
+      subtitle: 'Clothes & Personal Items',
+    },
+    {
+      id: 4,
+      title: 'Delhivery Protect',
+      icon: 'view_in_ar',
+      subtitle: 'Clothes & Personal Items',
+    },
+
+  ];
 }
